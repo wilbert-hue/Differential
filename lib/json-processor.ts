edd2fa80934a15203f1aa1de9564f62392589728
7717CMI <coherentdrive@gmail.com>
2026-04-09 19:02:40 +0530

    commit

--- lib/json-processor.ts
+++ lib/json-processor.ts
@@ -1472,7 +1472,7 @@ export async function processJsonDataAsync(
     
     // Build metadata
     const metadata: Metadata = {
-      market_name: ' Automotive Passenger Cars Differential Market',
+      market_name: ' Passenger Cars Differential Market',
       market_type: 'Market Analysis',
       industry: 'Healthcare & Pharmaceuticals',
       years: allYears,
